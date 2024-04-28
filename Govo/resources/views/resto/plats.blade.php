9a73da2fd541e60012e8fa80e9187e9af92a6162
ahmed-hounati <hounatiahmed@gmail.com>
2024-04-28 18:34:28 +0100

    last commit

--- Govo/resources/views/resto/plats.blade.php
+++ Govo/resources/views/resto/plats.blade.php
@@ -65,50 +65,50 @@
                         </button>
                         <!-- Mobile menu dropdown
                             <div class="absolute top-0 left-0 w-full">
-                              <div class="p-5 bg-white border rounded shadow-sm">
+                                <div class="p-5 bg-white border rounded shadow-sm">
                                 <div class="flex items-center justify-between mb-4">
-                                  <div>
+                                    <div>
                                     <a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
-                                      <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
+                                        <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
                                         <rect x="3" y="1" width="7" height="12"></rect>
                                         <rect x="3" y="17" width="7" height="6"></rect>
                                         <rect x="14" y="1" width="7" height="6"></rect>
                                         <rect x="14" y="11" width="7" height="12"></rect>
-                                      </svg>
-                                      <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
+                                        </svg>
+                                        <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
                                     </a>
-                                  </div>
-                                  <div>
+                                    </div>
+                                    <div>
                                     <button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                                      <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
                                         <path
-                                          fill="currentColor"
-                                          d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                                            fill="currentColor"
+                                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
                                         ></path>
-                                      </svg>
+                                        </svg>
                                     </button>
-                                  </div>
+                                    </div>
                                 </div>
                                 <nav>
-                                  <ul class="space-y-4">
+                                    <ul class="space-y-4">
                                     <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
                                     <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
                                     <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
                                     <li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
                                     <li><a href="/" aria-label="Sign in" title="Sign in" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Sign in</a></li>
                                     <li>
-                                      <a
+                                        <a
                                         href="/"
                                         class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                                         aria-label="Sign up"
                                         title="Sign up"
-                                      >
+                                        >
                                         Sign up
-                                      </a>
+                                        </a>
                                     </li>
-                                  </ul>
+                                    </ul>
                                 </nav>
-                              </div>
+                                </div>
                             </div>
                             -->
                     </div>
@@ -148,24 +148,27 @@
                     <div class="text-center">
                         <p class="text-3xl">{{ $plat->price }}$</p>
                     </div>
-                    <div class="flex justify-between">
-                        <a href="{{ route('plats.edit', $plat->id) }}"
-                            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Update</a>
-                        <form method="POST" action="{{ route('plats.destroy', $plat->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit"
-                                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
-                        </form>
-                    </div>
+                    @if (!Auth()->user()->ban)
+                        <div class="flex justify-between">
+                            <a href="{{ route('plats.edit', $plat->id) }}"
+                                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Update</a>
+                            <form method="POST" action="{{ route('plats.destroy', $plat->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                            </form>
+                        </div>
+                    @endif
                 </div>
             @endforeach
         </div>
 
-
-        <a href="/plats/create"
-            class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 fixed bottom-4 right-4"><i
-                class="fa-solid fa-plus fa-xl"></i></a>
+        @if (!Auth()->user()->ban)
+            <a href="/plats/create"
+                class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 fixed bottom-4 right-4"><i
+                    class="fa-solid fa-plus fa-xl"></i></a>
+        @endif
 
 
     </section>
