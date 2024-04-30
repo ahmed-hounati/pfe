20b6b8138460c62d68f924ab8161e419170ef38f
ahmed-hounati <hounatiahmed@gmail.com>
2024-04-30 09:00:23 +0100

    last commit

--- Govo/resources/views/user/card.blade.php
+++ Govo/resources/views/user/card.blade.php
@@ -83,41 +83,41 @@
                                         <rect x="3" y="17" width="7" height="6"></rect>
                                         <rect x="14" y="1" width="7" height="6"></rect>
                                         <rect x="14" y="11" width="7" height="12"></rect>
-                                      </svg>
-                                      <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
+                                        </svg>
+                                        <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
                                     </a>
-                                  </div>
-                                  <div>
+                                    </div>
+                                    <div>
                                     <button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                                      <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
                                         <path
-                                          fill="currentColor"
-                                          d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                                            fill="currentColor"
+                                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
                                         ></path>
-                                      </svg>
+                                        </svg>
                                     </button>
-                                  </div>
+                                    </div>
                                 </div>
                                 <nav>
-                                  <ul class="space-y-4">
+                                    <ul class="space-y-4">
                                     <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
                                     <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
                                     <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
                                     <li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
                                     <li><a href="/" aria-label="Sign in" title="Sign in" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Sign in</a></li>
                                     <li>
-                                      <a
+                                        <a
                                         href="/"
                                         class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                                         aria-label="Sign up"
                                         title="Sign up"
-                                      >
+                                        >
                                         Sign up
-                                      </a>
+                                        </a>
                                     </li>
-                                  </ul>
+                                    </ul>
                                 </nav>
-                              </div>
+                                </div>
                             </div>
                             -->
                     </div>
